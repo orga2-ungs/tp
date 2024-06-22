c35989126fd4564bfd80dfcd9047c44be9041a45
Angelo <angelofaria060@gmail.com>
2024-06-21 21:04:40 -0300

    testing

--- run-test.sh
+++ run-test.sh
@@ -1,2 +1,2 @@
 gcc -o test calculadora-aux.c tests/LimpiarInputTest.c
-./test.exe
+./test
--- tests/LimpiarInputTest.c
+++ tests/LimpiarInputTest.c
@@ -18,38 +18,44 @@ void test_LimpiarInput() {
     assert(operador == '-');
     assert(num2 == 2);
 
-    // // Test 3: Multiplication with spaces
-    // LimpiarInput(" 4 * 5 ", &num1, &operador, &num2);
-    // assert(num1 == 4);
-    // assert(operador == '*');
-    // assert(num2 == 5);
+    // Test 3: Multiplication with spaces
+    LimpiarInput(" 4 * 5 ", &num1, &operador, &num2);
+    assert(num1 == 4);
+    assert(operador == '*');
+    assert(num2 == 5);
 
-    // // Test 4: Division
-    // LimpiarInput("6/3", &num1, &operador, &num2);
-    // assert(num1 == 6);
-    // assert(operador == '/');
-    // assert(num2 == 3);
+    // Test 4: Division
+    LimpiarInput("6/3", &num1, &operador, &num2);
+    assert(num1 == 6);
+    assert(operador == '/');
+    assert(num2 == 3);
 
-    // // Test 5: Negative numbers
-    // LimpiarInput("-1+2", &num1, &operador, &num2);
-    // assert(num1 == -1);
-    // assert(operador == '+');
-    // assert(num2 == 2);
+    // Test 5: Negative numbers
+    LimpiarInput("-1+2", &num1, &operador, &num2);
+    assert(num1 == -1);
+    assert(operador == '+');
+    assert(num2 == 2);
 
-    // // Test 6: Both operands negative
-    // LimpiarInput("-3*-2", &num1, &operador, &num2);
-    // assert(num1 == -3);
-    // assert(operador == '*');
-    // assert(num2 == -2);
+    // Test 5B: Segundo operando negativo
+    LimpiarInput("1+-2", &num1, &operador, &num2);
+    assert(num1 == 1);
+    assert(operador == '+');
+    assert(num2 == -2);
 
-    // // Test 7: Previous output use (simulated by skipping the first number)
+    // Test 6: Both operands negative
+    LimpiarInput("-3*-2", &num1, &operador, &num2);
+    assert(num1 == -3);
+    assert(operador == '*');
+    assert(num2 == -2);
+
+    // Test 7: Previous output use (simulated by skipping the first number)
     // int resultado_anterior = 10;
     // LimpiarInput("+5", &num1, &operador, &num2);
     // assert(num1 == 10);
     // assert(operador == '+');
     // assert(num2 == 5);
 
-    // // Test 8: Invalid operation
+    // Test 8: Invalid operation
     // LimpiarInput("invalid", &num1, &operador, &num2);
     // assert(num1 == 0);
     // assert(operador == '+');
