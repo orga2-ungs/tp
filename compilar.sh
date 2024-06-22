def09bf8c1f4e2e13478c3b97a46cba6f5235f56
Angelo <angelofaria060@gmail.com>
2024-06-22 17:47:11 -0300

    commit

--- compilar.sh
+++ compilar.sh
@@ -1,5 +1,5 @@
 nasm -f elf calculadora.asm -o calculadora-asm.o
+gcc -m32 -c calculadora-aux.c -o calculadora-aux.o
 gcc -m32 -c calculadora.c -o calculadora-c.o
-gcc -m32 calculadora-c.o calculadora-asm.o -o calculadora
+gcc -m32 calculadora-c.o calculadora-aux.o calculadora-asm.o -o calculadora
 ./calculadora
-
